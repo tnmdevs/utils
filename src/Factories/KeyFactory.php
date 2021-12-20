2f296ff1453340929c634f35c9a1f357ffa839d6
Saul Chelewani <saulchelewani@gmail.com>
2021-12-20 22:12:45 +0200

    key factory

---
+++ src/Factories/KeyFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace TNM\Utils\Factories;
+
+class KeyFactory
+{
+    private int $length;
+    private bool $numeric;
+
+    public function __construct(int $length = 32, bool $numeric = false)
+    {
+        $this->length = $length;
+        $this->numeric = $numeric;
+    }
+
+    public function make(): string
+    {
+        $characters = $this->numeric ? "0123456789" : '23456789abcdefghijkmnpqrstuvwxyz';
+
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $this->length; $i++)
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+
+        return $randomString;
+    }
+}
