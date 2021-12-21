f173f309b5fed462a0e6beb72e3648f0fce280ff
Saul Chelewani <saulchelewani@gmail.com>
2021-12-21 08:05:55 +0200

    refactor key factory

--- src/Factories/KeyFactory.php
+++ src/Factories/KeyFactory.php
@@ -5,23 +5,19 @@ namespace TNM\Utils\Factories;
 class KeyFactory
 {
     private int $length;
-    private bool $numeric;
+    private string $characters;
 
     public function __construct(int $length = 32, bool $numeric = false)
     {
         $this->length = $length;
-        $this->numeric = $numeric;
+        $this->characters = $numeric ? "0123456789" : '0123456789abcdefghijkmnpqrstuvwxyz';
     }
 
     public function make(): string
     {
-        $characters = $this->numeric ? "0123456789" : '0123456789abcdefghijkmnpqrstuvwxyz';
-
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < $this->length; $i++)
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-
-        return $randomString;
+        return substr(str_shuffle(str_repeat(
+            $this->characters,
+            ceil($this->length / strlen($this->characters))
+        )), 1, $this->length);
     }
 }
