53fc0613e4e570c015a5189f97d5cf640421f4a3
Saul Chelewani <saulchelewani@gmail.com>
2023-07-31 11:03:21 +0200

    replace timestamp with uniqid

--- src/Factories/TransactionIDFactory.php
+++ src/Factories/TransactionIDFactory.php
@@ -4,34 +4,15 @@ namespace TNM\Utils\Factories;
 
 class TransactionIDFactory
 {
-    private int $length;
     private ?string $prefix;
 
     public function __construct(int $length = 18, ?string $prefix = null)
     {
-        $this->length = $length;
         $this->prefix = $prefix;
     }
 
     public function make(): string
     {
         return uniqid($this->prefix);
-    }
-
-    private function getTime(): int
-    {
-        return (int)round(microtime(true) * 1000);
-    }
-
-    private function getSuffix(): string
-    {
-        $characters = "0123456789";
-
-        $charactersLength = strlen($characters);
-        $randomString = '';
-        for ($i = 0; $i < ($this->length - 13 - strlen($this->prefix)); $i++) {
-            $randomString .= $characters[rand(0, $charactersLength - 1)];
-        }
-        return $randomString;
     }
 }
